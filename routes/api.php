61f35d731aee3f4426d993eb8ef9eea9f95468d0
bonfim <gabriel.bonfim111@gmail.com>
2026-05-20 17:08:27 -0300

    refact: rota com caminho errado

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ClimaController;
 
-Route::apiResource('climas', ClimaController::class);
+Route::apiResource('/climas', ClimaController::class);
